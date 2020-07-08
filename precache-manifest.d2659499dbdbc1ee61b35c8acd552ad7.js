a4f75e037e781de96dcfd53cfd4e3afce2eb7d6e
BardAlexandrovskiy <alexandr8558@gmail.com>
2020-07-08 15:07:24 +0300

    Updates

---
+++ precache-manifest.d2659499dbdbc1ee61b35c8acd552ad7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4feca0ce2f27a299b3294402651fceb",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "22db70bbbd2967a818fc",
+    "url": "/resume/static/js/2.5fb49ae9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/resume/static/js/2.5fb49ae9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc919b6200cb88627799",
+    "url": "/resume/static/js/main.1875a6c4.chunk.js"
+  },
+  {
+    "revision": "8c1d914a1677cea53979",
+    "url": "/resume/static/js/runtime-main.75d8c91e.js"
+  }
+]);
