db500d7f87223cfec27e835d74437cf7dd51b1da
paranoidb <alexandr8558@gmail.com>
2020-07-09 03:00:57 +0300

    Updates

---
+++ precache-manifest.45c5b477f05a5860931a285acdfdef29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21c94ff13b29281574b014637ad2baf9",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "568ed9b9ce846e02ac8b",
+    "url": "/resume/static/js/2.5c0b097c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/resume/static/js/2.5c0b097c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a72aa73f74013a6858d",
+    "url": "/resume/static/js/main.aee17f18.chunk.js"
+  },
+  {
+    "revision": "8c1d914a1677cea53979",
+    "url": "/resume/static/js/runtime-main.75d8c91e.js"
+  },
+  {
+    "revision": "d135a07c8e84d0dd241270c85f56c146",
+    "url": "/resume/static/media/avatar.d135a07c.jpg"
+  }
+]);
